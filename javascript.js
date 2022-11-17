7cffafaa2ccfb88792830f3af29a731a2da692ba
VitorKalil <vitorkalil@yahoo.com>
2022-11-17 10:25:05 +0100

    added some shadow testing with jQuery

--- javascript.js
+++ javascript.js
@@ -1,2 +1,13 @@
-console.log(document.querySelector(".box"))
-document.querySelector(".box").style.paddingTop = "200px"
+var caixa = $(".box")
+caixa.css("background-color","red").css("text-align","center")
+$(document).mousemove(function(e){
+    let sombraH = 40 - 80*e.pageX/$(window).width()
+    let sombraV = 40 - 80*e.pageY/$(window).height()
+    let distSombra = (((sombraH**2) + (sombraV**2))**0.5)
+    sombraH = sombraH+"px"
+    sombraV = sombraV+"px"
+    distSombra = distSombra+"px"
+    let sombraString = sombraH + " "+ sombraV + " " + distSombra+ " 10px #888"
+    caixa.html("Shadow Testing")
+    caixa.css("box-shadow",sombraString)
+})
